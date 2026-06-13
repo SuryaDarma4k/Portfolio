8179a9eb4a412c513e12894e4ae49be8f16777c2
SuryaDarma4k <suryadarma4k@gmail.com>
2026-06-13 14:47:06 +0700

    Contact Section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import DataImage from "./data";
-import {listTools} from "./data";
+import { listTools, listProyek } from "./data";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
               src={DataImage.HeroImage}
               alt="Hero Image"
               className="w-10 rounded-md"
+              loading="lazy"
             />
             <q>Kode yang indah, lahir dari ketekunan.👌</q>
           </div>
@@ -48,6 +49,7 @@ function App() {
           src={DataImage.HeroImage}
           alt="Hero Image"
           className="w-[500px] md:ml-auto"
+          loading="lazy"
         />
       </div>
 
@@ -58,6 +60,7 @@ function App() {
             src={DataImage.HeroImage}
             alt="Image"
             className="w-12 rounded-md mb-10 sm:hidden"
+            loading="lazy"
           />
           <p className="text-base/loose mb-10">
             Hi, perkenalkan saya Diky Prayoga, seorang Full Stack Web Developer
@@ -71,6 +74,7 @@ function App() {
               src={DataImage.HeroImage}
               alt="Image"
               className="w-12 rounded-md sm:block hidden"
+              loading="lazy"
             />
             <div className="flex items-center gap-6">
               <div>
@@ -94,24 +98,61 @@ function App() {
             Berikut ini beberapa tools yang biasa dipakai untuk pembuatan
             website ataupun Design
           </p>
-          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-2)
-          grid-cols-1 gap-4">
+          <div
+            className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-2)
+          grid-cols-1 gap-4"
+          >
             {listTools.map((tool) => (
-              <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}>
-              <img
-                src={tool.gambar}
-                alt="Tools Image"
-                className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
-              />
-              <div>
-                <h4 className="font-bold">{tool.nama}</h4>
-                <p className="opacity-50">{tool.keterangan}</p>
+              <div
+                className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group"
+                key={tool.id}
+              >
+                <img
+                  src={tool.gambar}
+                  alt="Tools Image"
+                  className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
+                  loading="lazy"
+                />
+                <div>
+                  <h4 className="font-bold">{tool.nama}</h4>
+                  <p className="opacity-50">{tool.keterangan}</p>
+                </div>
               </div>
-            </div>
             ))}
           </div>
         </div>
       </div>
+      {/* Proyek */}
+      <div className="proyek mt-32 py-10">
+        <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
+        <p className="text-base/loose text-center opacity-50">
+          Berikut Ini Beberapa Proyek yang telah saya buat
+        </p>
+        <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {listProyek.map(proyek => (
+            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
+              <img src={proyek.gambar} alt="Proyek image" loading="lazy"/>
+              <div>
+                <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
+                <p className="text-base/loose mb-4">{proyek.desk}</p>
+                <div className="flex flex-wrap gap-2">
+                  {proyek.tools.map((tool, index) =>(
+                    <p className="py-1 px-3 border border-zinc-500 
+                    bg-zinc-600 rounded-md font-semibold" key={index}>
+                      {tool}
+                    </p>
+                  ))}
+                </div>
+                <div className="mt-8 text-center">
+                  <a href="#" className="bg-violet-700 p-3 rounded-lg block
+                  border-zinc-600 hover:bg-violet-600">Lihat Website</a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Proyek */}
     </>
   );
 }
