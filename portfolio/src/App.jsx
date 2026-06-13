dcfc6396b061859cb87ed2afb4536aec0bf22f0b
SuryaDarma4k <suryadarma4k@gmail.com>
2026-06-13 14:23:14 +0700

    Tentang Section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,7 @@
-import DataImage from './data';
+import DataImage from "./data";
+import {listTools} from "./data";
 
 function App() {
-
   return (
     <>
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
@@ -17,7 +17,7 @@ function App() {
           <h1 className="text-5xl/tight font-bold mb-6">
             Hi, My Name is Harel Surya Darma
           </h1>
-          <p className='text-base/loose mb-6 opacity-50'>
+          <p className="text-base/loose mb-6 opacity-50">
             Mahasiswa Teknik Informatika Politeknik Caltex Riau yang adaptif
             dengan fondasi kuat dalam rekayasa perangkat lunak dan analisis
             data. Memiliki pengalaman mengembangkan aplikasi web menggunakan
@@ -27,21 +27,93 @@ function App() {
             bisnis. Siap berkontribusi secara fleksibel baik dalam tim
             pengembangan perangkat lunak maupun analitik data
           </p>
-          <div className='flex items-center gap-4'>
-            <a href="#" className='bg-violet-700 p-4 rounded-2xl hover:bg-violet-700'>
-              Download CV 
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a
+              href="#"
+              className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-700"
+            >
+              Download CV
               <i class="ri-download-line ri-lg"></i>
             </a>
-            <a href="#" className='bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-700'>
-              Lihat Proyek 
+            <a
+              href="#"
+              className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-700"
+            >
+              Lihat Proyek
               <i class="ri-arrow-down-line ri-lg"></i>
             </a>
           </div>
         </div>
-        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] ml-auto" />
+        <img
+          src={DataImage.HeroImage}
+          alt="Hero Image"
+          className="w-[500px] md:ml-auto"
+        />
+      </div>
+
+      {/* Tentang */}
+      <div className="tentang mt-32 py-10">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+          <img
+            src={DataImage.HeroImage}
+            alt="Image"
+            className="w-12 rounded-md mb-10 sm:hidden"
+          />
+          <p className="text-base/loose mb-10">
+            Hi, perkenalkan saya Diky Prayoga, seorang Full Stack Web Developer
+            dan Designer untuk UI/UX Design maupun Product Digital, Saya percaya
+            bahwa desain dan fungsionalitas harus berjalan beriringan, sehingga
+            setiap proyek yang saya kembangkan tidak hanya terlihat menarik
+            tetapi juga memberikan pengalaman pengguna yang optimal.
+          </p>
+          <div className="flex items-center justify-between">
+            <img
+              src={DataImage.HeroImage}
+              alt="Image"
+              className="w-12 rounded-md sm:block hidden"
+            />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">
+                  45<span className="text-violet-500">+</span>
+                </h1>
+                <p>Project Selesai</p>
+              </div>
+              <div>
+                <h1 className="text-4xl mb-1">
+                  4<span className="text-violet-500">+</span>
+                </h1>
+                <p>Tahun Pengalaman</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="tools mt-32">
+          <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
+          <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">
+            Berikut ini beberapa tools yang biasa dipakai untuk pembuatan
+            website ataupun Design
+          </p>
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-2)
+          grid-cols-1 gap-4">
+            {listTools.map((tool) => (
+              <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}>
+              <img
+                src={tool.gambar}
+                alt="Tools Image"
+                className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
+              />
+              <div>
+                <h4 className="font-bold">{tool.nama}</h4>
+                <p className="opacity-50">{tool.keterangan}</p>
+              </div>
+            </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
