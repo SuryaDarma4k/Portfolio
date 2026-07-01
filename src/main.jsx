2fa5790ae1a671d993354fee12d326c1b9986850
SuryaDarma4k <suryadarma4k@gmail.com>
2026-07-01 10:37:52 +0700

    Update navbar section

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './App.css';
 import App from './App.jsx'
 import { Navbar } from './components/Navbar.jsx';
 import 'remixicon/fonts/remixicon.css';
 import Footer from './components/Footer.jsx';
 import { PreLoader } from './components/PreLoader.jsx';
 import 'animate.css';
+
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
